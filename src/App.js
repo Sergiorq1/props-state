65eb614f94aed1cfc98e383dc9422145de6a1a00
Sergiorq1 <sergiorq12016@gmail.com>
2023-01-23 22:40:10 -0700

    can add and delete counters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter'
+import { useState } from 'react';
 
 function App() {
+  const [ count, setCount ] = useState([ 4, 8, 0 ])
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {count.map((val, i) => (
+        <Counter
+          //second argument, iteration
+          label={`Counter: ${i+1}`}
+          //first argument, value 
+          value={val}
+          //delete function
+          deleteCount={() =>{
+            const newCount = [ ...count]
+            newCount[i] = count.slice(i, 1)
+            setCount(newCount)
+          }}
+          //increment function
+          increment={() => {
+            //copied array
+            const newCount = [ ...count]
+            //modified new array
+            newCount[i] = val + 1
+            // updated state, causing component to rerender
+            setCount(newCount)
+          }}
+          //same as increment, except opposite
+          decrement={() => {
+            const newCount = [ ...count]
+            newCount[i] = val -1
+            setCount(newCount)
+          }}
+        />
+      ))}
+      <h2>Total: {count.reduce((acc, val) => acc + val, 0)}</h2>
+      <button onClick={() => {
+        const newCount = [ ...count]
+        newCount.push(0)
+        setCount(newCount)
+      }}>Add counter</button>
+
     </div>
   );
 }
